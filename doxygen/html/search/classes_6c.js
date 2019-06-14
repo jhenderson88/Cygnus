2d07e900c186f08f0764982ef0dee96af5bf0b16
Jack Henderson <henderson2534@gmail.com>
2019-06-14 09:22:17 -0700

    Add LitMatrixElements

--- doxygen/html/search/classes_6c.js
+++ doxygen/html/search/classes_6c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['litbranchingratio',['LitBranchingRatio',['../classLitBranchingRatio.html',1,'']]],
   ['litlifetime',['LitLifetime',['../classLitLifetime.html',1,'']]],
+  ['litmatrixelement',['LitMatrixElement',['../classLitMatrixElement.html',1,'']]],
   ['litmixingratio',['LitMixingRatio',['../classLitMixingRatio.html',1,'']]]
 ];
